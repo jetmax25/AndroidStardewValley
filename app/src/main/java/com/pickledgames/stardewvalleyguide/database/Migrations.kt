5e944ca396dae725221f414bc55c0f3a4e8e4682
Dennis Deng <dennisdeng2002@yahoo.com>
2018-10-27 15:46:31 -0400

    add default empty string to migration

--- app/src/main/java/com/pickledgames/stardewvalleyguide/database/Migrations.kt
+++ app/src/main/java/com/pickledgames/stardewvalleyguide/database/Migrations.kt
@@ -7,7 +7,7 @@ object Migrations {
 
     val MIGRATION_1_2 = object : Migration(1, 2) {
         override fun migrate(database: SupportSQLiteDatabase) {
-            database.execSQL("ALTER TABLE Farm ADD COLUMN fishes TEXT NOT NULL")
+            database.execSQL("ALTER TABLE Farm ADD COLUMN fishes TEXT NOT NULL DEFAULT ''")
         }
     }
 }
